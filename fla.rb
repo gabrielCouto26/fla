923283b4934174f6c0279b8de29a2ae0e4ba94b6
Gabriel Couto <gabrielcouto26@gmail.com>
2021-10-09 23:42:39 -0300

    final

--- fla.rb
+++ fla.rb
@@ -1,12 +1,10 @@
 require 'httparty'
 
-retorno = []
-
 url = 'https://webws.365scores.com/web/games/current/?appTypeId=5&langId=31&timezoneName=America/Sao_Paulo&userCountryId=21&competitors=1215&lastUpdateId=3837248352'
-
 response = HTTParty.get(url)
-
 games = response.parsed_response["games"]
+
+jogos = []
 
 games.each { |game|
   competicao = game["competitionDisplayName"]
@@ -16,12 +14,14 @@ games.each { |game|
   score_mandante = game["homeCompetitor"]["score"]
   score_visitante = game["awayCompetitor"]["score"]
   
-  retorno.push({
+  hash = {
     "competicao" => competicao,
     "data" => data,
     "mandante" => "#{mandante} #{score_mandante}",
     "visitante" => "#{visitante} #{score_visitante}"
-  })
+  }
+
+  jogos.push hash
 } 
 
-retorno
+print jogos
