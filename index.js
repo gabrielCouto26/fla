a9f0392e1d2101f47df750c399f68b7653afe331
Gabriel Couto <gabrielcouto26@gmail.com>
2022-08-09 22:41:02 -0300

    possibilita definir qtd de jogos que deseja ver

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
 const axios = require('axios')
+const process = require('process')
 
+const buscaJogos = async () => {
+  let url = 'https://webws.365scores.com/web/games/current/?appTypeId=5&langId=31&timezoneName=America/Sao_Paulo&userCountryId=21&competitors=1215&lastUpdateId=3837248352'
+  let jogos = []
 
-url = 'https://webws.365scores.com/web/games/current/?appTypeId=5&langId=31&timezoneName=America/Sao_Paulo&userCountryId=21&competitors=1215&lastUpdateId=3837248352'
+  await axios
+    .get(url)
+    .then(({ data }) => {
+      let { games } = data
+      let qtdJogos  = process.argv[2] || games.length
 
+      for(let i = 0; i < qtdJogos; i++){
+        let game           = games[i]
+        let competicao     = game.competitionDisplayName
+        let data           = game.startTime
+        let mandante       = game.homeCompetitor.name
+        let visitante      = game.awayCompetitor.name
+        let scoreMandante  = game.homeCompetitor.score
+        let scoreVisitante = game.awayCompetitor.score
 
-async function getJogos(){
-    let jogos = []
+        let temp = data.split('T')
+        temp[0]  = temp[0].split('-').reverse().join('-')
+        temp[1]  = temp[1].split('-')[0]
+        data     = temp.join(' ')
 
-    await axios
-            .get(url)
-            .then(response => {
-                // console.log(response.data.games)
-                let games = response.data.games
-                for(let game of games){
-                    let competicao = game.competitionDisplayName
-                    let data = game.startTime
-                    // let status = game.statusText == "Programação" ? "Agendado" : game.statusText
-                    let mandante = game.homeCompetitor.name
-                    let visitante = game.awayCompetitor.name
-                    let scoreMandante = game.homeCompetitor.score
-                    let scoreVisitante = game.awayCompetitor.score
-
-
-                    let temp = data.split('T')
-                    temp[0] = temp[0].split('-').reverse().join('-')
-                    temp[1] = temp[1].split('-')[0]
-                    data = temp.join(' ')
-
-
-                    let jogo = {
-                        competicao,
-                        data,
-                        // status,
-                        mandante: `${mandante} ${scoreMandante}`,
-                        visitante: `${visitante} ${scoreVisitante}` 
-                    }
-                    jogos.push(jogo)
-                }
-                console.log(JSON.stringify(jogos, null, 2))
-            })
-            .catch(error => {
-                console.log(error)
-            })
+        let jogo = {
+          competicao,
+          data,
+          mandante: `${mandante} ${scoreMandante}`,
+          visitante: `${visitante} ${scoreVisitante}` 
+        }
+        jogos.push(jogo)
+      }
+      console.log(JSON.stringify(jogos, null, 2))
+    })
+    .catch(error => {
+      console.log(error)
+    })
 }
 
-getJogos()
+buscaJogos()
